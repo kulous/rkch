69e1f731978974c62b4c72d016450f60c7b4e958
Micah Schaefer <micah@kulo.us>
2014-12-25 15:10:21 +0000

    added database and redis modules

--- server.js
+++ server.js
@@ -5,6 +5,55 @@ var session = require('express-session');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+
+var RedisStore = require('connect-redis')(session);
+var session_store = new RedisStore;
+
+/* database 
+var mongoose = require('mongoose');
+var dbhost = 'db1-prod.kulo.us';
+var dbport = '27017';
+
+
+
+mongoose.connect('mongodb://' + dbhost + ':' + dbport + '/test');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console,'connection error: '));
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var userSchema = new Schema({
+	name: String,
+	email: String,
+	createdDate: { 
+		type: Date ,
+		default: Date.now 
+	}
+})
+
+var channelSchema = new Schema({
+	name: String,
+	createdDate: { 
+		type: Date ,
+		default: Date.now 
+	}
+})
+var messageSchema = new  Schema({
+	user: {
+		type: ObjectId,
+		ref: 'userSchema',
+	},
+	content: String,
+	channel: {
+		type: ObjectId,
+		ref: 'channelSchema'
+	}
+})
+		
+*/
+
 /* in memory storage 
 var redis = require('redis'),
 	memstore = redis.createClient();
@@ -13,11 +62,21 @@ memstore.on('error', function (err) {
 });
 memstore.set('userlist',[]);
 */
+
+
+
 /* session handling */
 app.use(session({
 	secret: 's883ndkhg1713bdub',
-	resave: false,
-	saveUninitialized: true
+	resave: true,
+	saveUninitialized: true,
+	cookie:{
+		path: '/',
+		httpOnly: true,
+		secure: false,
+		maxAge: null
+	},
+	store: new RedisStore()
 }));
 
 /* oAuth 
@@ -36,34 +95,38 @@ app.get('/style', function(req, res){
 });
 app.get('/', function(req, res){
 	sess=req.session;
-	if(!sess.user)
-		/* this is where we would login, lets set up as a gues */
+	if(!sess){
+		/* this is where we would login, lets set up as a guest 
 		usercount ++;
 		sess.user='Guest' + usercount;
 		/* give a warm welcome */
 		/*io.emit('chat message','New user '+sess.user + ' joined!');*/
-		console.log('no user session');
+	}
+	else{
+	}
 	res.sendFile(__dirname + '/index.html');
 });
 
 /* web socket connection */
 io.on('connection', function(socket){
-	io.emit('nt_userlist',users);
-	/*if(sess.user)
+	var data = socket.request;
+
+	io.emit('log','connection from ' + data.connection.remoteAddress + ':' + data.connection.remotePort);
+	
+	/*
+	if(sess){
 		io.emit('user join',sess.user);
-		console.log('user ' + sess.user + ' connected');
-		io.emit('chat message','New user '+sess.user + ' joined!');
+	}
 	*/
-    
 	socket.on('disconnect',function(){
-		io.emit('user leave',sess.user);
-		console.log('user ' + sess.user + ' disconnected');
-		io.emit('chat message','User '+sess.user + ' left!');
+		io.emit('user leave','username');
+		console.log('user ' + 'username' + ' disconnected');
+		io.emit('chat message','User '+ 'username' + ' left!');
 	});
 	socket.on('chat message', function(msg){
 	/*console.log(socket.request.connection);*/
 	/*io.emit('chat message', socket.request.connection.remoteAddress + '> ' + msg);*/
-	io.emit('chat message', sess.user + '# ' + msg);
+	io.emit('chat message', 'username' + '#<br/>' + msg);
 	});
 });
 
